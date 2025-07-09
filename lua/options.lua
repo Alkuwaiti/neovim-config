f0030a04a563c2897c5b0a9898632a7282b37a78
qasim <alkuwaitiqasim@gmail.com>
2025-07-09 13:41:40 +0300

    auto attach lsp to tf files

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,11 @@ require "nvchad.options"
 vim.opt.relativenumber = true
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
+
+-- auto attach terraformls lsp to .tf files.
+vim.filetype.add {
+  extension = {
+    tf = "terraform",
+    tfvars = "terraform",
+  },
+}
